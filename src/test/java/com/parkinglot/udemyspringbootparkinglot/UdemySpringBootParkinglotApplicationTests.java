737eb130b6d5182362d0dfa0b2ca81c77369f608
Lukasz Spyra <lukasz.spyrka@gmail.com>
2020-03-14 14:08:37 +0100

    Created app using Spring Initializr

---
+++ src/test/java/com/parkinglot/udemyspringbootparkinglot/UdemySpringBootParkinglotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parkinglot.udemyspringbootparkinglot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemySpringBootParkinglotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
